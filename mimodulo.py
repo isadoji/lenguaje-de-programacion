ccc1d232ea3bf864f20235cdb6806d00e5f532ba
isadoji <isadoji@gmail.com>
2026-05-04 10:18:30 -0700

    update OOP

--- mimodulo.py
+++ mimodulo.py
@@ -1,4 +1,4 @@
-
+ 
 class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
@@ -6,3 +6,18 @@ class Persona:
 
     def __str__(self):
         return f"Nombre: {self.nombre}, Edad: {self.edad}"
+    
+    def saludar_v2(self):
+        return f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años."
+    
+    def despedirse_v2(self):
+        return f"Adiós, me llamo {self.nombre} y tengo {self.edad} años."
+
+'''
+def saludar_v2(nombre):
+    return f"Hola, mi nombre es {nombre}."
+    
+def despedirse_v2(nombre,edad=0):
+        return f"Adiós, me llamo {nombre} y tengo {edad} años."
+
+'''
